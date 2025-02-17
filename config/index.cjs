7de1350bbddd5b579f9ec176a8f0a3a09f481173
yeziking <47059892+yeziking@users.noreply.github.com>
2025-02-17 11:48:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -28,7 +28,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'onIJR7P0ennneAcBUeQq-skVF7bk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	VWrmzbFyt_VoqlnldFRHv7oqBlKgN7ZSFv7vBtDZIsk',
+      useTemplateId: 'ZdglYiAZ7A6Ob91I0jjwxGHq1-gjxJwR3UluooFya_U',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '3-30',
       festivals: [
@@ -56,7 +56,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	VWrmzbFyt_VoqlnldFRHv7oqBlKgN7ZSFv7vBtDZIsk',
+  CALLBACK_TEMPLATE_ID: 'ZdglYiAZ7A6Ob91I0jjwxGHq1-gjxJwR3UluooFya_U',
 
   CALLBACK_USERS: [
     {
