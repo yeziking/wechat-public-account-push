1fb9be0bdce1f3ffe8be1aeeebd28dd76aa8c468
yeziking <47059892+yeziking@users.noreply.github.com>
2025-02-18 18:17:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,7 +14,27 @@ const USER_CONFIG = {
   APP_SECRET: '0e243bd5cb6e84e69c9f8e48bdc27fa6',
 
   // 天行API配置
-  TIAN_API: '268a723e949a009c40503553984ebd6d', // 你的天行API密钥
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '268a723e949a009c40503553984ebd6d',
+    
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+  
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: true,
+  
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 3,
+  
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 3,
+  
+    // 全网热搜榜展示类型，默认展示概要信息: ['title': 仅展示标题, 'default': 展示概要信息]
+    networkHotType: 'default',
+  },
+  
   
   PROVINCE: ' ',
   CITY: '南宁',
@@ -53,7 +73,7 @@ const USER_CONFIG = {
 
     {
       // 想要发送的人的名字
-      name: '测试幸福',
+      name: '测试幸福1',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'onIJR7P0ennneAcBUeQq-skVF7bk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
